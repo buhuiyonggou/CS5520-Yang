c5da3b83d6d19dfda098fae4aa6af818b4b237c9
buhuiyonggou <yangli4944@gmail.com>
2023-07-06 15:26:39 -0700

    add name variable

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = "Yang";
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on {name} app!</Text>
       <Text>Change!</Text>
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
