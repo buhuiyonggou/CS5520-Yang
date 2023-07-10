1fb2b29468ab0e35292bebd5e3bd51df4b510cb8
buhuiyonggou <yangli4944@gmail.com>
2023-07-10 16:20:02 -0700

    update class3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 import React, { useState } from 'react';
@@ -7,18 +7,25 @@ import React, { useState } from 'react';
 export default function App() {
   const appName = "CS5520";
   const [inputText, setInputText] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
     // this function is called when the text input changes
   // inside it update the state variable inputText
   function handleChangeText(changedText){
     // update the state variable inputText
     // console.log(changedText);
     setInputText(changedText);
+    hideModal();
+  }
+
+  function hideModal(){
+    setModalVisible(false);
   }
 
   return (
     <View style={styles.container}>
-      <Header name={appName} title="NEU summer course"/>
-        <Input changeTextCallBack={handleChangeText}/>
+      <Header name={appName} />
+        <Button title="Add A Goal" onPress={()=>{setModalVisible(true)}}/>
+        <Input changeTextCallBack={handleChangeText} modalVisible={modalVisible}/>
         <Text>{inputText}</Text>
       <StatusBar style="auto"/>
     </View>
--- components/Input.js
+++ components/Input.js
@@ -1,22 +1,42 @@
-import { View, TextInput, StyleSheet} from 'react-native'
-import React from 'react';
+import { View, TextInput, Button, Modal, Image, StyleSheet} from 'react-native'
+import React, {useState} from 'react';
 
-const Input = ({changeTextCallBack}) => {
+const Input = ({changeTextCallBack, modalVisible}) => {
+  const [text, setText] = useState('');
+  
+  function storeText(changedText){
+    // console.log(changedText);
+    setText(changedText);
+  }
 
   return (
-    <View>
-    <TextInput style={styles.input}
-    onChangeText={changeTextCallBack}
-    />
-    </View>
+    <Modal visible={modalVisible} animationType='slide'>
+      <View style={styles.container}>
+        <Image source={{uri:'https://img.freepik.com/premium-photo/anime-girl-watching-sunset-3d-illustration_717906-1415.jpg?w=2000'}} style={styles.image}/>
+        <Image source={require("../assets/pic.png")} style={styles.image}/>
+        <TextInput style={styles.input} onChangeText={storeText} value={text}/>
+        <Button title='Confirm' onPress={()=> {changeTextCallBack(text)}}/>
+       <Button title="Cancel" onPress={()=>{setText(""); changeTextCallBack("") }}/>
+      </View>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   input: {
     borderBottomWidth: 1,
     width: 200,
     height: 40,
+  },
+  image: {
+    width: 100,
+    height: 100,
   }
 });
 
